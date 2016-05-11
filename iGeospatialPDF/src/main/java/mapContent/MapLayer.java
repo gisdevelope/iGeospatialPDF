754f1025b0892b0aa73d6d9ded08c4ccecb4382c
DaGri <david.griegel.1991@gmail.com>
2016-05-11 14:32:12 +0200

    Updated MapLayer.java
    + added BoundingBox.java attribute
    + added getters and setters
    # updated JavaDoc

--- iGeospatialPDF/src/main/java/mapContent/MapLayer.java
+++ iGeospatialPDF/src/main/java/mapContent/MapLayer.java
@@ -1,20 +1,47 @@
 package mapContent;
 
+import geo.BoundingBox;
+
 /**
  * Abstract parental class for all children of a {@link MapLayer}.
  * 
  * @author DaGri
  * @since 03.05.2016
  */
-public abstract class MapLayer { // TODO : FILL CLASS
+public abstract class MapLayer {
 
 	// ATTRIBUTES
+
+	/**
+	 * The {@link BoundingBox} of this {@link MapLayer} that defines the covered
+	 * area of the surface of the earth.
+	 */
+	private BoundingBox bbox;
 
 	// CONSTRUCTORS
 
 	// METHODS
 
 	// GETTERS AND SETTERS
+
+	/**
+	 * Returns the {@link BoundingBox} of this {@link MapLayer}.
+	 *
+	 * @return the bbox as {@link BoundingBox}
+	 */
+	public BoundingBox getBbox() {
+		return bbox;
+	}
+
+	/**
+	 * Sets the {@link BoundingBox} of this {@link MapLayer}.
+	 *
+	 * @param bbox
+	 *            the bbox to set
+	 */
+	public void setBbox(BoundingBox bbox) {
+		this.bbox = bbox;
+	}
 
 	// OTHERS
 }
