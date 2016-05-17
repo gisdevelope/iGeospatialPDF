ff10c367c11beaf4afe5b1e0d2f358d5cda89320
DaGri <david.griegel.1991@gmail.com>
2016-05-17 10:57:28 +0200

    Added Enumeration for the supported versions of the servers
    + added enumeration ServerVersion.java

---
+++ iGeospatialPDF/src/main/java/resources/ServerVersion.java
@@ -0,0 +1,12 @@
+package resources;
+
+/**
+ * Enumeration to provide correct input of web map service (WMS) and web feature
+ * service (WFS) version.
+ * 
+ * @author DaGri
+ * @since 13.05.2016
+ */
+public enum ServerVersion {
+	v_1_1_0, v_1_3_0
+}
