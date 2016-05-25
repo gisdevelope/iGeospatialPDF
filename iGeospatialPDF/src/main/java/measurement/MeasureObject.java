a9c4c82b3dad8ed8b05a2ed2629f30b8ad474099
DaGri <david.griegel.1991@gmail.com>
2016-05-25 14:59:21 +0200

    Added Measure Classes
    + added class MeasureAngle.java
    + added class MeasureObject.java
    + added class MeasurePoint.java
    # all created classes are empty for now

---
+++ iGeospatialPDF/src/main/java/measurement/MeasureObject.java
@@ -0,0 +1,21 @@
+package measurement;
+
+/**
+ * Class to TODO
+ * 
+ * @author DaGri
+ * @since 24.05.2016
+ *
+ */
+public class MeasureObject {
+
+	// ATTRIBUTES
+
+	// CONSTRUCTORS
+
+	// METHODS
+
+	// GETTERS AND SETTERS
+
+	// OTHERS
+}
---
+++ iGeospatialPDF/src/main/java/measurement/MeasurePoint.java
@@ -0,0 +1,21 @@
+package measurement;
+
+/**
+ * Class to TODO
+ * 
+ * @author DaGri
+ * @since 24.05.2016
+ *
+ */
+public class MeasurePoint extends MeasureObject{
+
+	// ATTRIBUTES
+
+	// CONSTRUCTORS
+
+	// METHODS
+
+	// GETTERS AND SETTERS
+
+	// OTHERS
+}
